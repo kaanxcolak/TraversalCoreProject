64ed2535097e5897be70511b48317a332e9c1fa6
Kaan <kaancolak3458@gmail.com>
2025-08-09 23:51:53 +0300

    updated

--- DataAccessLayer/Abstract/IDestinationDal.cs
+++ DataAccessLayer/Abstract/IDestinationDal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,8 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-    public interface IDestinationDal
+    public interface IDestinationDal:IGenericDal<Destination>
     {
-        //
+         
     }
 }
