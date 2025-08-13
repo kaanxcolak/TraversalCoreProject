05d0f00f06dfa2b21e3da81d5204494af2185ec9
Kaan <kaancolak3458@gmail.com>
2025-08-13 19:24:19 +0300

    updated

--- BusinessLayer/Concrete/AboutManager.cs
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,37 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-    public class AboutManager
+    public class AboutManager : IAboutService
     {
+        IAboutDal _aboutDal;
+
+        public AboutManager(IAboutDal aboutDal)
+        {
+            _aboutDal = aboutDal;            
+        }
+        public List<About> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TAdd(About t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(About t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public About TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(About t)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
