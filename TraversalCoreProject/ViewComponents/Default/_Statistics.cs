cedcc348719595a7fdbbb410b7688d802a32a6a6
Kaan <kaancolak3458@gmail.com>
2025-08-28 19:51:38 +0300

    created

---
+++ TraversalCoreProject/ViewComponents/Default/_Statistics.cs
@@ -0,0 +1,14 @@
+﻿using BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _Statistics : ViewComponent 
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
