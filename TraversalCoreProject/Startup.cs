109f800b55287c4863b9bab120b63cfd8348dbd9
Kaan <kaancolak3458@gmail.com>
2025-08-29 19:49:51 +0300

    updated

--- TraversalCoreProject/Startup.cs
+++ TraversalCoreProject/Startup.cs
@@ -50,7 +50,7 @@ namespace TraversalCoreProject
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Default}/{action=Index}/{id?}");
             });
         }
     }
