01a4da76c5c8c376830bfede45b48899ef2acd41
Kaan <kaancolak3458@gmail.com>
2025-10-16 19:35:26 +0300

    updated

--- TraversalCoreProject/Controllers/LoginController.cs
+++ TraversalCoreProject/Controllers/LoginController.cs
@@ -1,22 +1,33 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TraversalCoreProject.Models;
 
 namespace TraversalCoreProject.Controllers
 {
     [AllowAnonymous]
     public class LoginController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        public LoginController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         [HttpGet]
         public IActionResult SignUp()
         {
             return View();
         }
 
-        //[HttpPost]
-        //public IActionResult SignUp()
-        //{
-        //    return View();
-        //}
+        [HttpPost]
+        public async Task<IActionResult> SignUp(UserRegisterViewModel p)
+        {
+            return View();
+        }
 
         [HttpGet]
         public IActionResult SignIn()
