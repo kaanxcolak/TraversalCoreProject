af5ec264639c51c76f15f587d096756fa9631fc8
Kaan <kaancolak3458@gmail.com>
2025-10-17 19:21:09 +0300

    update

--- TraversalCoreProject/Controllers/LoginController.cs
+++ TraversalCoreProject/Controllers/LoginController.cs
@@ -26,7 +26,30 @@ namespace TraversalCoreProject.Controllers
         [HttpPost]
         public async Task<IActionResult> SignUp(UserRegisterViewModel p)
         {
-            return View();
+            AppUser appUser = new AppUser()
+            {
+                Name = p.Name,
+                Surname = p.Surname,
+                Email = p.Mail,
+                UserName = p.Username
+            };
+
+            if(p.Password == p.ConfirmPassword)
+            {
+                var result = await _userManager.CreateAsync(appUser,p.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("SignIn");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View(p);
         }
 
         [HttpGet]
