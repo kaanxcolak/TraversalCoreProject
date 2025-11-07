870f478d24559553e1fe228ce615ae7d24ec50ec
Kaan <kaancolak3458@gmail.com>
2025-11-07 18:48:06 +0300

    updated

--- TraversalCoreProject/Models/UserSignInViewModel.cs
+++ TraversalCoreProject/Models/UserSignInViewModel.cs
@@ -1,6 +1,14 @@
-﻿namespace TraversalCoreProject.Models
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace TraversalCoreProject.Models
 {
     public class UserSignInViewModel
     {
+        [Required(ErrorMessage ="Ltfen kullanıcı adını giriniz")]
+        public string username { get; set; }
+
+        [Required(ErrorMessage ="Ltfen şifrenizi giriniz")]
+        public string password { get; set; }
     }
 }
