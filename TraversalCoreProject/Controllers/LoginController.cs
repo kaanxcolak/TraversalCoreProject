89831fbec06a0025a1309d9bf316d0660b598a0a
Kaan <kaancolak3458@gmail.com>
2025-11-11 20:08:08 +0300

    created

--- TraversalCoreProject/Controllers/LoginController.cs
+++ TraversalCoreProject/Controllers/LoginController.cs
@@ -68,7 +68,7 @@ namespace TraversalCoreProject.Controllers
                 var result = await _signInManager.PasswordSignInAsync(p.username, p.password, false, true);
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("Index", "Destination");
+                    return RedirectToAction("Index", "Profile",new { area = "Member" });
                 }
                 else
                 {
